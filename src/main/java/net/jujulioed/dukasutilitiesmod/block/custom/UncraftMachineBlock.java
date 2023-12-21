073d954cdca58ffae4fa45e587653c63a711f499
juujulio <julioeduardon9@hotmail.com>
2023-12-21 13:10:38 -0300

    Fixing some "Uncraft Machine" issues
     - Fixed the texture transparency issue;
     - Fixed the model render issue.
    
    Signed-off-by: juujulio <julioeduardon9@hotmail.com>

--- src/main/java/net/jujulioed/dukasutilitiesmod/block/custom/UncraftMachineBlock.java
+++ src/main/java/net/jujulioed/dukasutilitiesmod/block/custom/UncraftMachineBlock.java
@@ -23,7 +23,7 @@ import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class UncraftMachineBlock extends BaseEntityBlock {
-    public static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 12, 16);
+    public static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 15, 16);
     public UncraftMachineBlock(Properties pProperties) {
         super(pProperties);
     }
